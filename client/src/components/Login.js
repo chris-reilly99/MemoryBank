028be2730bbf61dff562c4422b25808672a2eff0
kkrogs <kourtney.krogman@gmail.com>
2022-06-05 13:24:30 -0600

    form signup and login styling coming along

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -3,19 +3,30 @@ import { Col } from 'react-bootstrap';
 import { Row } from 'react-bootstrap';
 import { Form } from 'react-bootstrap';
 
+
+
+
 function Login () {
 return (
 
 <Form className="mt-5">
-  <Form.Group className="mb-3" controlId="formGroupUsername">
+
+    <Col xs={7}>
+        <Form.Group className="mb-3" controlId="formGroupUsername">
     <Form.Label>Username</Form.Label>
     <Form.Control type="email" placeholder="Enter Username" />
   </Form.Group>
-  <Form.Group className="mb-3" controlId="formGroupPassword">
+    </Col>
+    <Col xs={7}>
+       <Form.Group className="mb-3" controlId="formGroupPassword">
     <Form.Label>Password</Form.Label>
     <Form.Control type="password" placeholder="Password" />
   </Form.Group>
+    </Col>
+   
+
 </Form>
+
 
 
 )
