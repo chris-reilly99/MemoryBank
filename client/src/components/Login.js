9af3caa3c99295c31baaa55ac4828a7ceb53c341
kkrogs <kourtney.krogman@gmail.com>
2022-06-05 13:07:00 -0600

    login, signup, homepage shell complete

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Col } from 'react-bootstrap';
 import { Row } from 'react-bootstrap';
 import { Form } from 'react-bootstrap';
-import { Button } from 'react-bootstrap';
 
 function Login () {
 return (
