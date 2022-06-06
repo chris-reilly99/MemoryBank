6b90a1ca83c653e5f017296ad0dcca6cc320ba8f
kkrogs <kourtney.krogman@gmail.com>
2022-06-05 18:50:26 -0600

    forms centered

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -11,13 +11,13 @@ return (
 
 <Form className="mt-5">
 
-    <Col xs={7}>
+    <Col xs={7} md={{ span: 4, offset: 4 }}>
         <Form.Group className="mb-3" controlId="formGroupUsername">
     <Form.Label>Username</Form.Label>
     <Form.Control type="email" placeholder="Enter Username" />
   </Form.Group>
     </Col>
-    <Col xs={7}>
+    <Col xs={7} md={{ span: 4, offset: 4 }}>
        <Form.Group className="mb-3" controlId="formGroupPassword">
     <Form.Label>Password</Form.Label>
     <Form.Control type="password" placeholder="Password" />
--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -8,34 +8,39 @@ function SignUp () {
 return (
 
 <Form className="mt-5">
-  <Row className="mb-3">
+  <Col xs={7} md={{ span: 4, offset: 4 }}>
     <Form.Group as={Col} controlId="formGridEmail">
       <Form.Label>First Name</Form.Label>
       <Form.Control type="First Name" placeholder="Enter First Name" />
     </Form.Group>
-
+    </Col>
+    
+    <Col xs={7} md={{ span: 4, offset: 4 }}>
     <Form.Group as={Col} controlId="formGridPassword">
       <Form.Label>Last Name</Form.Label>
       <Form.Control type="Last Name" placeholder="Enter Last Name" />
     </Form.Group>
-  </Row>
-
+    </Col>
+    
+  
+  <Col xs={7} md={{ span: 4, offset: 4 }}>
   <Form.Group className="mb-3" controlId="formGridAddress1">
     <Form.Label>Username</Form.Label>
     <Form.Control placeholder="Username" />
   </Form.Group>
-
+  </Col>
+  <Col xs={7} md={{ span: 4, offset: 4 }}>
   <Form.Group className="mb-3" controlId="formGridAddress2">
     <Form.Label>Password</Form.Label>
     <Form.Control placeholder="Password" />
   </Form.Group>
-
-
+  </Col>
+  <Col xs={7} md={{ span: 4, offset: 4 }}>
   <Button variant="primary" type="submit">
     Submit
   </Button>
+  </Col>
 </Form>
-
 
 )
 
